8b4cd765549d5ed83cac1557985084820147a7e2
a86398960-alt <a86398960@gmail.com>
2025-09-01 22:50:23 +0200

    Updated package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,10 +35,6 @@ export default {
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
       },
-      spacing: {
-        '18': '4.5rem',
-        '88': '22rem',
-      }
     },
   },
   plugins: [],
